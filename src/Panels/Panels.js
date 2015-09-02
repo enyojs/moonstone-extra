9467584962d0809fa6d0a006c24eb3ac91b94ec6
Serhei Alyshchuk <serhei.alyshchuk@lge.com>
2015-09-02 17:22:00 +0300

    ENYO-2136: Panel disappears when tapping the left side of the first panel
    Enyo-DCO-1.1-Signed-off-by: Serge Alischuk <serhei.alyshchuk@lge.com>

--- src/Panels/Panels.js
+++ src/Panels/Panels.js
@@ -831,17 +831,17 @@ module.exports = kind(
 			return;
 		}
 
-		// If tapped on not current panel (breadcrumb), go to that panel
-		if (oEvent.breadcrumbTap && oEvent.index !== this.getIndex()) {
-			this.setIndex(oEvent.index);
+		// If tapped on breadcrambs area (which is located in the left side of panel)
+		if (oEvent.originator === this.$.breadcrumbs) {
+			if (this.showing && (this.useHandle === true) && this.handleShowing) {
+				this.hide();
+			}
+		} else {
+			// If tapped on breadcrumb, go to that panel
+			if (oEvent.breadcrumbTap && oEvent.index !== this.getIndex()) {
+				this.setIndex(oEvent.index);
+			}
 		}
-	},
-
-	/**
-	* @private
-	*/
-	shouldHide: function (oEvent) {
-		return (oEvent.originator === this.$.clientWrapper || (oEvent.originator instanceof Panel && this.isPanel(oEvent.originator)));
 	},
 
 	/**
