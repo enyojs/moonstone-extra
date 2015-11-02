3505c63a16bef4cf3e672cf8a377b0dc52037c65
Roy Sutton <roy.sutton@lge.com>
2015-11-02 15:18:06 -0800

    Make sure close button exists before modifying
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton <roy.sutton@lge.com>

--- src/Panels/Panels.js
+++ src/Panels/Panels.js
@@ -1238,7 +1238,9 @@ module.exports = kind(
 		this.toIndex = index;
 
 		// Turn on the close-x so it's ready for the next panel; if hasCloseButton is true
-		this.$.appClose.customizeCloseButton({showing: this.hasCloseButton});
+		if (this.$.appClose) {
+			this.$.appClose.customizeCloseButton({showing: this.hasCloseButton});
+		}
 		this.notifyPanels('initPanel');
 
 		// Ensure any VKB is closed when transitioning panels
