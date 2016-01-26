c75c7f9070df208812c32d04034f03ebfbcfacb7
jeremy.thomas <jeremy.thomas@lge.com>
2016-01-26 14:27:01 -0500

    Spot when navigating from app close button

--- src/Panels/Panels.js
+++ src/Panels/Panels.js
@@ -1031,6 +1031,9 @@ module.exports = kind(
 					Spotlight.spot(Spotlight.getLastControl());
 				}
 				return true;
+			} else if (sender instanceof ApplicationCloseButton && this.$.breadcrumbs) {
+				Spotlight.spot(this.$.breadcrumbs);
+				return true;
 			}
 		}
 	},
