f25297080cd14a8f547a5d5175ba6a2ec0605401
Ryan Duffy <ryan.duffy@lge.com>
2016-02-23 15:03:02 -0600

    migrate modules to moonstone
    Issue: PLAT-16760
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- src/VideoFullscreenToggleButton/VideoFullscreenToggleButton.js
+++ src/VideoFullscreenToggleButton/VideoFullscreenToggleButton.js
@@ -1,87 +1,10 @@
+var logger = require('enyo/logger');
+logger.warn('moonstone-extra/VideoFullscreenToggleButton has moved to "moonstone". Please update your code.');
+
 /**
-* Contains the declaration for the {@link module:moonstone-extra/VideoFullscreenToggleButton~VideoFullscreenToggleButton} kind.
+* Temporary proxy for {@link module:moonstone/VideoFullscreenToggleButton}
+*
+* @deprecated
 * @module moonstone-extra/VideoFullscreenToggleButton
 */
-
-require('moonstone-extra');
-
-var
-	kind = require('enyo/kind');
-
-var
-	$L = require('../i18n'),
-	IconButton = require('moonstone/IconButton');
-
-/**
-* {@link module:moonstone-extra/VideoFullscreenToggleButton~VideoFullscreenToggleButton} is a specialized {@link module:moonstone/IconButton~IconButton};
-* when placed inside a {@link module:moonstone-extra/VideoPlayer~VideoPlayer}, the button may be tapped to toggle
-* the video player's fullscreen state.
-*
-* @class VideoFullscreenToggleButton
-* @extends module:moonstone/IconButton~IconButton
-* @ui
-* @public
-*/
-module.exports = kind(
-	/** @lends module:moonstone-extra/VideoFullscreenToggleButton~VideoFullscreenToggleButton */ {
-
-	/**
-	* @private
-	*/
-	name: 'moon.VideoFullscreenToggleButton',
-
-	/**
-	* @private
-	*/
-	kind: IconButton,
-
-	/**
-	* @private
-	*/
-	icon : 'exitfullscreen',
-
-	/**
-	* @private
-	*/
-	small: false,
-
-	/**
-	* @private
-	*/
-	classes : 'moon-icon-exitfullscreen-font-style',
-
-	/**
-	* @private
-	*/
-	events: {
-		/**
-		* {@link module:moonstone-extra/VideoPlayer~VideoPlayer#onRequestToggleFullscreen}
-		*/
-		onRequestToggleFullscreen:''
-	},
-
-	/**
-	* @private
-	*/
-	handlers: {
-		/**
-		* @fires module:moonstone-extra/VideoPlayer~VideoPlayer#onRequestToggleFullscreen
-		*/
-		ontap: 'doRequestToggleFullscreen'
-	},
-
-	// Accessibility
-
-	/**
-	* @private
-	*/
-	ariaObservers: [
-		{path: 'icon', method: function () {
-			if (this.icon === 'exitfullscreen') {
-				this.set('accessibilityLabel', $L('original screen'));
-			} else {
-				this.set('accessibilityLabel', $L('full screen'));
-			}
-		}}
-	]
-});
+module.exports = require('moonstone/VideoFullscreenToggleButton');
