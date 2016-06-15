4879cde555cd62fd3063399190abdbca0d4aafcf
rajyavardhanp <rajyavardhan.p@lge.com>
2016-06-15 11:56:46 +0530

    PLAT-21868-RajyavardhanReddyP
    updating based on the review comments.
    Enyo-DCO-1.1-Signed-off-by: Rajyavardhan Reddy P
    <rajyavardhan.p@lge.com>

--- src/VideoTransportSlider/VideoTransportSlider.js
+++ src/VideoTransportSlider/VideoTransportSlider.js
@@ -789,8 +789,8 @@ module.exports = kind(
 		var val;
 		if (this.tappable && !this.disabled) {
 			val = this.transformToVideo(this.calcKnobPosition(e));
-			this.sendSeekEvent(val);
 			this._updateKnobPosition(val);
+			this.sendSeekEvent(val);
 			return true;
 		}
 	},
